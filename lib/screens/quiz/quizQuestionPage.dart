b9fd513ccbca34ff644d358dcf8687ad9b1796b5
VinaySavla <savlavinay77@gmail.com>
2024-03-13 23:49:30 +0530

    fixed quiz result storing

--- lib/screens/quiz/quizQuestionPage.dart
+++ lib/screens/quiz/quizQuestionPage.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -7,44 +8,81 @@ import 'package:mindcare/screens/home/home.dart';
 import 'package:mindcare/widgets/quizRadioButton.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:confetti/confetti.dart';
-import 'dart:convert';
 import 'package:http/http.dart' as http;
+
+import 'package:emoji_rating_bar/emoji_rating_bar.dart';
+import 'package:mindcare/const/colors.dart';
+import 'package:provider/provider.dart';
+
 class quizQuestionPagea extends StatefulWidget {
   quizQuestionPagea({super.key});
 
   @override
   State<quizQuestionPagea> createState() => _quizQuestionPageaState();
-  
 }
 
 class _quizQuestionPageaState extends State<quizQuestionPagea> {
-  Map<String, int> quizResults = {}; // Store the results of all questions
+  String quizResultsJson = '[]'; // Store the results of all questions as a JSON string
 
-  Future<void> updateQuizResult(String id, int result) async {
-    final response = await http.patch(
-      Uri.parse('https://mindcare-app.onrender.com/api/quiz/'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, int>{
-        'pk': int.parse(id),
-        'result': result,
-      }),
+  final ValueNotifier<String> _quizResultsNotifier = ValueNotifier('[]');
+
+  // Future<void> updateQuizResult(String resultsJson) async {
+  //   try {
+  //       print(resultsJson);
+  //     final response = await http.put(
+  //       // Uri.parse('https://mindcare-app.onrender.com/api/quiz/'),
+  //       Uri.parse('http://localhost:8000/api/quiz/'),
+  //       // headers: <String, String>{
+  //       //   'Content-Type': 'application/json; charset=UTF-8',
+  //       // },
+  //       body: resultsJson,
+  //     );
+
+  //     if (response.statusCode == 200) {
+  //       print('Quiz results updated successfully.');
+  //     } else {
+  //       throw Exception('Failed to update quiz results. Server responded with status code ${response.statusCode}.');
+  //     }
+  //   } catch (e, stackTrace) {
+  //     print('Failed to update quiz results. Error: $e');
+  //     print('Stack trace: $stackTrace');
+  //     throw Exception('Failed to update quiz results.');
+  //   }
+  // }
+
+Future<void> updateQuizResult(String resultsJson) async {
+  try {
+    final response = await http.put(
+        Uri.parse('https://mindcare-app.onrender.com/api/quiz/'),
+      // Uri.parse('http://localhost:8000/api/quiz/'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Accept': '*/*'
+        },
+      body: resultsJson,
     );
+  print('Updating quiz results...');
 
     if (response.statusCode == 200) {
-      print('Quiz result updated successfully.');
+      print('Quiz results updated successfully.');
     } else {
-      throw Exception('Failed to update quiz result.');
+      throw Exception('Failed to update quiz results. Server responded with status code ${response.statusCode}.');
     }
+  } catch (e, stackTrace) {
+    print('Failed to update quiz results. Error: $e');
+    print('Stack trace: $stackTrace');
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Failed to update quiz results. Please try again later.')),
+    );
   }
+}
 
   Future<List<dynamic>> fetchQuizQuestions() async {
     final response = await http.get(Uri.parse('https://mindcare-app.onrender.com/api/quiz/'));
 
     if (response.statusCode == 200) {
       // return jsonDecode(response.body);
-      
+
       List<dynamic> data = jsonDecode(response.body);
       return data.take(10).toList();
     } else {
@@ -64,13 +102,85 @@ class _quizQuestionPageaState extends State<quizQuestionPagea> {
         } else {
           return Column(
             children:[ ...snapshot.data!.map((question) {
-              return quizRadioButton(
-                questionsNum: question['id'], 
-                questions: question['question'], 
-                result: question['result'],
-                onResultChange: (result) {
-                  quizResults[question['id']] = result; // Store the result when it changes
-                }
+              return Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                    decoration: BoxDecoration(
+                      color: lightTextColor3,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Text(
+                question['id'].toString() +
+                                        ". " +
+                                        question['question'].toString(),style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            StatefulBuilder(
+                              builder: (context, setState) {
+                                return EmojiRatingBar(
+                                  rating: 3,
+                                  onRateChange: (rating) {
+                                    if (rating != 3) { // Check if the rating is not the default value
+                                      setState(() {
+                                        List<dynamic> results = jsonDecode(quizResultsJson);
+                                        results.add({
+                                          "user_profile_id":1,
+                                          "question_id": question['id'],
+                                          "question": question['question'],
+                                          "result": rating,
+                                        });
+                                        quizResultsJson = jsonEncode(results);
+                                        _quizResultsNotifier.value = quizResultsJson;
+                                      });
+                                    }
+                                    print(rating);
+                                  },
+                                  isReadOnly: false,
+                                  spacing: 10,
+                                  size: 40,
+                                  selectedSize: 45,
+                                  isShowTitle: true,
+                                  // isShowDivider: true,
+                                  titleStyle: TextStyle(
+                                    color: Colors.white70,
+                                    fontSize: 8,
+                                  ),
+                                  selectedTitleStyle: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 8,
+                                  ),
+                                  animationDuration: Duration(milliseconds: 300),
+                                  animationCurve: Curves.easeInOut,
+                                  ratingBarType: RatingBarType.mood,
+                                  applyColorFilter: true,
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               );
             }).toList(),
             AnimatedButton(
@@ -84,18 +194,17 @@ class _quizQuestionPageaState extends State<quizQuestionPagea> {
               backgroundColor: lightTextColor3,
               borderColor: Colors.white,
               borderRadius: 40,
-              borderWidth: 2, onPress: () async { 
-                for (var entry in quizResults.entries) {
-                  await updateQuizResult(entry.key, entry.value);
-                }
+              borderWidth: 2, onPress: () async {
+                print(quizResultsJson);
+                await updateQuizResult(quizResultsJson);
                 Navigator.push(
                   context,
                   PageTransition(
                     duration: const Duration(milliseconds: 500),
                     type: PageTransitionType.fade,
-                    child:  quizSubmitPage(),
+                    child: quizSubmitPage(quizResultsJson: quizResultsJson),
                   ),
-                );
+               );
                },
             ),
             SizedBox(height: 20,)
@@ -106,69 +215,76 @@ class _quizQuestionPageaState extends State<quizQuestionPagea> {
     );
   }
 }
+
 class quizSubmitPage extends StatefulWidget {
-  quizSubmitPage({super.key});
+  final String quizResultsJson;
+
+  const quizSubmitPage({Key? key, required this.quizResultsJson}) : super(key: key);
 
   @override
   State<quizSubmitPage> createState() => _quizSubmitPageState();
-  
-
-
 }
 
 class _quizSubmitPageState extends State<quizSubmitPage> {
-    final confettiController = ConfettiController(duration: const Duration(seconds: 3));
-    void initState() {  
-      confettiController.play();
-      super.initState();
-    }
-    @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    confettiController.dispose();
+  final confettiController = ConfettiController(duration: const Duration(seconds: 3));
+
+  @override
+  void initState() {
+    confettiController.play();
+    super.initState();
   }
+
+  @override
+  void dispose() {
+    confettiController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<dynamic> quizResultsList = jsonDecode(widget.quizResultsJson);
+
+    String jsonString = jsonEncode(quizResultsList);
+
+    print(jsonString);
+
     return Scaffold(
-      
-    backgroundColor: Colors.transparent,
+      backgroundColor: Colors.transparent,
       body: SafeArea(
         child: Container(
-            height: MediaQuery.of(context).size.height - 35,
-            width: MediaQuery.of(context).size.width,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment(0.8, 1),
-                colors: <Color>[
-                  Color.fromARGB(255, 0, 0, 0),
-                  Color.fromARGB(255, 0, 55, 67),
-                  Color.fromARGB(255, 0, 91, 111),
-                ],
-                tileMode: TileMode.mirror,
-              ),
+          height: MediaQuery.of(context).size.height - 35,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment(0.8, 1),
+              colors: <Color>[
+                Color.fromARGB(255, 0, 0, 0),
+                Color.fromARGB(255, 0, 55, 67),
+                Color.fromARGB(255, 0, 91, 111),
+              ],
+              tileMode: TileMode.mirror,
             ),
-        child:SingleChildScrollView(
-          child: Column(
-            children: [
-              ConfettiWidget(
-            blastDirection: pi / 2,
-            confettiController: confettiController,
-            shouldLoop: true,
           ),
-              Container(
-                height: MediaQuery.of(context).size.height ,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                
-                    const Text("Your Score is 5/7", style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),),
-                    const SizedBox(height: 20,),
-                    AnimatedButton(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                ConfettiWidget(
+                  blastDirection: pi / 2,
+                  confettiController: confettiController,
+                  shouldLoop: true,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Your Score is 5/7", style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),),
+                      const SizedBox(height: 20,),
+                      AnimatedButton(
                         height: 70,
                         width: 200,
                         text: 'Go to Home',
@@ -179,26 +295,26 @@ class _quizSubmitPageState extends State<quizSubmitPage> {
                         backgroundColor: lightTextColor3,
                         borderColor: Colors.white,
                         borderRadius: 50,
-                        borderWidth: 2, onPress: () { 
+                        borderWidth: 2, onPress: () {
                           Navigator.push(
-                                context,
-                                PageTransition(
-                                  duration: const Duration(milliseconds: 500),
-                                  type: PageTransitionType.fade,
-                                  child: const homePage(),
-                                  
-                                ),
-                              );
-                         },
+                            context,
+                            PageTransition(
+                              duration: const Duration(milliseconds: 500),
+                              type: PageTransitionType.fade,
+                              child: const homePage(),
+                            ),
+                          );
+                        },
                       ),
-              
-                  ],
+                    ],
+                  ),
+                  // TextColor: Colors.black,
+                  //       transitionType: Trans
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-            ),
       ),
     );
   }
--- lib/screens/quiz/quizStartPage.dart
+++ lib/screens/quiz/quizStartPage.dart
@@ -16,80 +16,80 @@ class _quizStartPageState extends State<quizStartPage> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-                     SizedBox(
-                  height: 150,
-                ),
-                Container(
-                  padding: const EdgeInsets.all(20),
-                  margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-                  decoration: BoxDecoration(
-                    color: lightTextColor3,
-                    borderRadius: BorderRadius.circular(30),
-                    
+        SizedBox(
+          height: 150,
+        ),
+        Container(
+          padding: const EdgeInsets.all(20),
+          margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+          decoration: BoxDecoration(
+            color: lightTextColor3,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: const Text(
+                  "Mind Quest",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(top: 20),
-                        child: const Text(
-                          "Mind Quest",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 20),
-                        child: const Text(
-                          "Answer the questions to get a better understanding of your mental health.",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                      // SizedBox(
-                      //   height: 300,
-                      // ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 20),
-                        // child: ElevatedButton(
-                        //   onPressed: () {
-                        //     Navigator.pushNamed(context, '/quiz1');
-                        //   },
-                        //   child: const Text(
-                        //     "Start Quiz",
-                        //     style: TextStyle(
-                        //       color: Colors.white,
-                        //       fontSize: 20,
-                        //     ),
-                        //   ),
-                        //   style: ElevatedButton.styleFrom(
-                        //     primary: color2,
-                        //     padding: const EdgeInsets.symmetric(
-                        //         horizontal: 50, vertical: 20),
-                        //     shape: RoundedRectangleBorder(
-                        //       borderRadius: BorderRadius.circular(30),
-                        //     ),
-                        //   ),
-                        // ),
-                        child: AnimatedButton(
-              height: 70,
-              width: 200,
-              text: 'Start Quiz',
-              isReverse: true,
-              selectedTextColor: Colors.black,
-              transitionType: TransitionType.LEFT_TO_RIGHT,
-              // textStyle: submitTextStyle,
-              backgroundColor: const Color.fromARGB(158, 0, 0, 0),
-              borderColor: Colors.white,
-              borderRadius: 50,
-              borderWidth: 2, onPress: () { 
-                Navigator.pushReplacement(
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: const Text(
+                  "Answer the questions to get a better understanding of your mental health.",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              // SizedBox(
+              //   height: 300,
+              // ),
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                // child: ElevatedButton(
+                //   onPressed: () {
+                //     Navigator.pushNamed(context, '/quiz1');
+                //   },
+                //   child: const Text(
+                //     "Start Quiz",
+                //     style: TextStyle(
+                //       color: Colors.white,
+                //       fontSize: 20,
+                //     ),
+                //   ),
+                //   style: ElevatedButton.styleFrom(
+                //     primary: color2,
+                //     padding: const EdgeInsets.symmetric(
+                //         horizontal: 50, vertical: 20),
+                //     shape: RoundedRectangleBorder(
+                //       borderRadius: BorderRadius.circular(30),
+                //     ),
+                //   ),
+                // ),
+                child: AnimatedButton(
+                  height: 70,
+                  width: 200,
+                  text: 'Start Quiz',
+                  isReverse: true,
+                  selectedTextColor: Colors.black,
+                  transitionType: TransitionType.LEFT_TO_RIGHT,
+                  // textStyle: submitTextStyle,
+                  backgroundColor: const Color.fromARGB(158, 0, 0, 0),
+                  borderColor: Colors.white,
+                  borderRadius: 50,
+                  borderWidth: 2,
+                  onPress: () {
+                    Navigator.pushReplacement(
                       context,
                       PageTransition(
                         duration: const Duration(milliseconds: 500),
@@ -97,14 +97,12 @@ class _quizStartPageState extends State<quizStartPage> {
                         child: quizPage(start: true),
                       ),
                     );
-               },
-            ),
-                      ),
-                    ],
-                  
-                  ),
-                )
-
+                  },
+                ),
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
