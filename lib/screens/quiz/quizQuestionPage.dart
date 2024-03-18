570b841ed4e8fafb62e00efbd34daad618b0a20f
Sanket-Ugale <sanketugale683@outlook.com>
2024-03-18 23:38:57 +0530

    Add loading_animation_widget dependency

--- lib/screens/quiz/quizQuestionPage.dart
+++ lib/screens/quiz/quizQuestionPage.dart
@@ -13,6 +13,7 @@ import 'package:http/http.dart' as http;
 import 'package:emoji_rating_bar/emoji_rating_bar.dart';
 import 'package:mindcare/const/colors.dart';
 import 'package:provider/provider.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class quizQuestionPagea extends StatefulWidget {
   quizQuestionPagea({super.key});
@@ -22,8 +23,9 @@ class quizQuestionPagea extends StatefulWidget {
 }
 
 class _quizQuestionPageaState extends State<quizQuestionPagea> {
-  String quizResultsJson = '[]'; // Store the results of all questions as a JSON string
-
+  String quizResultsJson =
+      '[]'; // Store the results of all questions as a JSON string
+  bool _isupdatingResult = false;
   final ValueNotifier<String> _quizResultsNotifier = ValueNotifier('[]');
 
   // Future<void> updateQuizResult(String resultsJson) async {
@@ -50,35 +52,68 @@ class _quizQuestionPageaState extends State<quizQuestionPagea> {
   //   }
   // }
 
-Future<void> updateQuizResult(String resultsJson) async {
-  try {
-    final response = await http.put(
-        Uri.parse('https://mindcare-app.onrender.com/api/quiz/'),
-      // Uri.parse('http://localhost:8000/api/quiz/'),
+  Future<void> updateQuizResult(String resultsJson) async {
+    setState(() {
+      _isupdatingResult = true;
+    });
+    try {
+      final response = await http.post(
+        Uri.parse('https://mindcare-app.onrender.com/api/quizresult/'),
+        // 
+        // Uri.parse('http://localhost:8000/api/quiz/'),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
-          'Accept': '*/*'
+          // 'Accept': '*/*'
         },
-      body: resultsJson,
-    );
-  print('Updating quiz results...');
+        body: resultsJson,
+      );
+      print('Updating quiz results...');
 
-    if (response.statusCode == 200) {
-      print('Quiz results updated successfully.');
-    } else {
-      throw Exception('Failed to update quiz results. Server responded with status code ${response.statusCode}.');
+      if (response.statusCode == 201) {
+        print('Quiz results updated successfully.');
+        ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          content: Row(
+            children: [
+              const Icon(Icons.check_circle, color: Colors.greenAccent),
+              const SizedBox(width: 10),
+              Text(
+                'Quiz results saved successfully.',
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
+          )       
+      );
+      
+      } else {
+        throw Exception(
+            'Failed to update quiz results. Server responded with status code ${response.body}.');
+      }
+    } catch (e, stackTrace) {
+      print('Failed to update quiz results. Error: $e');
+      print('Stack trace: $stackTrace');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            'Failed to update quiz results. Please try again later. $e',
+            style: TextStyle(color: color1),
+          ),
+        ),
+      );
     }
-  } catch (e, stackTrace) {
-    print('Failed to update quiz results. Error: $e');
-    print('Stack trace: $stackTrace');
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Failed to update quiz results. Please try again later.')),
-    );
+    setState(() {
+      _isupdatingResult = false;
+    });
+
   }
-}
 
   Future<List<dynamic>> fetchQuizQuestions() async {
-    final response = await http.get(Uri.parse('https://mindcare-app.onrender.com/api/quiz/'));
+    final response = await http
+        .get(Uri.parse('https://mindcare-app.onrender.com/api/quizquestion/'));
 
     if (response.statusCode == 200) {
       // return jsonDecode(response.body);
@@ -92,141 +127,192 @@ Future<void> updateQuizResult(String resultsJson) async {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<dynamic>>(
-      future: fetchQuizQuestions(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator(color: Colors.white,));
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else {
-          return Column(
-            children:[ ...snapshot.data!.map((question) {
-              return Column(
+    return _isupdatingResult
+        ? Center(
+            child: Container(
+              margin: EdgeInsets.only(top: 280.0),
+              child: Column(
                 children: [
-                  Container(
-                    padding: const EdgeInsets.all(20),
-                    margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-                    decoration: BoxDecoration(
-                      color: lightTextColor3,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Column(
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Text(
-                question['id'].toString() +
-                                        ". " +
-                                        question['question'].toString(),style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            StatefulBuilder(
-                              builder: (context, setState) {
-                                return EmojiRatingBar(
-                                  rating: 3,
-                                  onRateChange: (rating) {
-                                    if (rating != 3) { // Check if the rating is not the default value
-                                      setState(() {
-                                        List<dynamic> results = jsonDecode(quizResultsJson);
-                                        results.add({
-                                          "user_profile_id":1,
-                                          "question_id": question['id'],
-                                          "question": question['question'],
-                                          "result": rating,
-                                        });
-                                        quizResultsJson = jsonEncode(results);
-                                        _quizResultsNotifier.value = quizResultsJson;
-                                      });
-                                    }
-                                    print(rating);
-                                  },
-                                  isReadOnly: false,
-                                  spacing: 10,
-                                  size: 40,
-                                  selectedSize: 45,
-                                  isShowTitle: true,
-                                  // isShowDivider: true,
-                                  titleStyle: TextStyle(
-                                    color: Colors.white70,
-                                    fontSize: 8,
-                                  ),
-                                  selectedTitleStyle: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 8,
-                                  ),
-                                  animationDuration: Duration(milliseconds: 300),
-                                  animationCurve: Curves.easeInOut,
-                                  ratingBarType: RatingBarType.mood,
-                                  applyColorFilter: true,
-                                );
-                              },
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+                  LoadingAnimationWidget.fourRotatingDots(
+                      color: color1, size: 55),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Submitting...',
+                    style: TextStyle(color: color1),
                   ),
                 ],
-              );
-            }).toList(),
-            AnimatedButton(
-              height: 60,
-              width: 200,
-              text: 'Submit',
-              isReverse: true,
-              selectedTextColor: Colors.black,
-              transitionType: TransitionType.LEFT_TO_RIGHT,
-              // textStyle: submitTextStyle,
-              backgroundColor: lightTextColor3,
-              borderColor: Colors.white,
-              borderRadius: 40,
-              borderWidth: 2, onPress: () async {
-                print(quizResultsJson);
-                await updateQuizResult(quizResultsJson);
-                Navigator.push(
-                  context,
-                  PageTransition(
-                    duration: const Duration(milliseconds: 500),
-                    type: PageTransitionType.fade,
-                    child: quizSubmitPage(quizResultsJson: quizResultsJson),
-                  ),
-               );
-               },
+              ),
             ),
-            SizedBox(height: 20,)
-            ]
+          )
+        : FutureBuilder<List<dynamic>>(
+            future: fetchQuizQuestions(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Padding(
+                  padding: EdgeInsets.only(top: 280.0),
+                  child: Center(
+                    child: LoadingAnimationWidget.fourRotatingDots(
+                        color: color1, size: 55),
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Text(
+                  'Error: ${snapshot.error}',
+                  style: TextStyle(color: color1),
+                );
+              } else {
+                return Column(children: [
+                  ...snapshot.data!.map((question) {
+                    return Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(20),
+                          margin: const EdgeInsets.only(
+                              left: 20, right: 20, bottom: 20),
+                          decoration: BoxDecoration(
+                            color: lightTextColor3,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                        child: Text(
+                                          question['id'].toString() +
+                                              ". " +
+                                              question['question'].toString(),
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 20,
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                  StatefulBuilder(
+                                    builder: (context, setState) {
+                                      return EmojiRatingBar(
+                                        rating: 3,
+                                        onRateChange: (rating) {
+                                          if (rating != 3) {
+                                            // Check if the rating is not the default value
+                                            setState(() {
+                                              List<dynamic> results = jsonDecode(quizResultsJson);
+                                              Map<String, dynamic> newResult = {
+                                                "user_profile_id": 1,
+                                                "question_id": question['id'],
+                                                "answer": rating,
+                                              };
+
+                                              int index = results.indexWhere((result) =>
+                                                result['user_profile_id'] == newResult['user_profile_id'] &&
+                                                result['question_id'] == newResult['question_id']
+                                              );
+
+                                              if (index != -1) {
+                                                // A dictionary with the same user_profile_id and question_id already exists in the list.
+                                                // Update the answer in that dictionary.
+                                                results[index] = newResult;
+                                              } else {
+                                                // No dictionary with the same user_profile_id and question_id exists in the list.
+                                                // Add a new dictionary to the list.
+                                                results.add(newResult);
+                                              }
+
+                                              quizResultsJson = jsonEncode(results);
+                                              _quizResultsNotifier.value = quizResultsJson;
+                                            });
+                                          }
+                                        },
+                                        isReadOnly: false,
+                                        spacing: 10,
+                                        size: 40,
+                                        selectedSize: 45,
+                                        isShowTitle: true,
+                                        // isShowDivider: true,
+                                        titleStyle: const TextStyle(
+                                          color: Colors.white70,
+                                          fontSize: 8,
+                                        ),
+                                        selectedTitleStyle: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 8,
+                                        ),
+                                        animationDuration:
+                                            const Duration(milliseconds: 300),
+                                        animationCurve: Curves.easeInOut,
+                                        ratingBarType: RatingBarType.mood,
+                                        applyColorFilter: true,
+                                      );
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
+                  }).toList(),
+                  AnimatedButton(
+                    height: 60,
+                    width: 200,
+                    text: 'Submit',
+                    isReverse: true,
+                    selectedTextColor: Colors.black,
+                    transitionType: TransitionType.LEFT_TO_RIGHT,
+                    // textStyle: submitTextStyle,
+                    backgroundColor: lightTextColor3,
+                    borderColor: Colors.white,
+                    borderRadius: 40,
+                    borderWidth: 2,
+                    onPress: () async {
+                      print(quizResultsJson);
+                      await updateQuizResult(quizResultsJson);
+                      Navigator.push(
+                        context,
+                        PageTransition(
+                          duration: const Duration(milliseconds: 500),
+                          type: PageTransitionType.fade,
+                          child:
+                              quizSubmitPage(quizResultsJson: quizResultsJson),
+                        ),
+                      );
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  )
+                ]);
+              }
+            },
           );
-        }
-      },
-    );
   }
 }
 
 class quizSubmitPage extends StatefulWidget {
   final String quizResultsJson;
 
-  const quizSubmitPage({Key? key, required this.quizResultsJson}) : super(key: key);
+  const quizSubmitPage({Key? key, required this.quizResultsJson})
+      : super(key: key);
 
   @override
   State<quizSubmitPage> createState() => _quizSubmitPageState();
 }
 
 class _quizSubmitPageState extends State<quizSubmitPage> {
-  final confettiController = ConfettiController(duration: const Duration(seconds: 3));
+  final confettiController =
+      ConfettiController(duration: const Duration(seconds: 3));
 
   @override
   void initState() {
@@ -279,11 +365,16 @@ class _quizSubmitPageState extends State<quizSubmitPage> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Text("Your Score is 5/7", style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),),
-                      const SizedBox(height: 20,),
+                      const Text(
+                        "Your Score is 5/7",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
                       AnimatedButton(
                         height: 70,
                         width: 200,
@@ -295,7 +386,8 @@ class _quizSubmitPageState extends State<quizSubmitPage> {
                         backgroundColor: lightTextColor3,
                         borderColor: Colors.white,
                         borderRadius: 50,
-                        borderWidth: 2, onPress: () {
+                        borderWidth: 2,
+                        onPress: () {
                           Navigator.push(
                             context,
                             PageTransition(
